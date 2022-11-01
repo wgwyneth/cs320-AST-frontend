6f5ee9c4748125a6be87884bf71a1ec98ffba784
Chetan Goel <chetan.goel2019@gmail.com>
2022-11-01 14:08:02 -0400

    Login button functionality

--- src/Auth.js
+++ src/Auth.js
@@ -1,5 +1,20 @@
 import React from "react";
 import welcome from './assets/welcome_img.png';
+import  { redirect } from 'react-router-dom'
+
+async function login() {
+  var username = "Gerald_Cunningham@fluffybunnyconsulting.com";
+  var password = "cunninghamge"; 
+  const response = await fetch('http://localhost:9000/api/login', {
+    method: 'POST',
+    body: JSON.stringify({email: username, password: password}),
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+  });
+  response.json().then((jsonObj) => console.log(jsonObj));
+}
 
 export default function (props) {
   return (
@@ -25,7 +40,7 @@ export default function (props) {
             />
           </div>
           <div className="d-grid gap-2 mt-3">
-            <button type="submit" className="btn btn-primary">
+            <button type="submit" className="btn btn-primary" onClick={login()}>
               Submit
             </button>
           </div>
